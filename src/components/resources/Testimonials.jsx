5589a84acab70a4a47edf55f2df9e7f281e2f8b9
Sylvia Mumbi <mumbi099@gmail.com>
2025-09-04 23:59:01 +0300

    Add function to view next slide + content

--- src/components/resources/Testimonials.jsx
+++ src/components/resources/Testimonials.jsx
@@ -178,11 +178,82 @@ function Testimonials() {
     setCurrent((prev) => (prev === 0 ? totalSlides - 1 : prev - 1));
   };
 
-
+  const nextSlide = () => {
+    setCurrent((prev) => (prev === totalSlides - 1 ? 0 : prev + 1));
+  };
 
   return (
     <section className="bg-gray-50 py-16 px-6">
-   
+         
+        <div className="max-w-6xl mx-auto grid md:grid-cols-3 gap-10 items-center">
+      
+        <div className="md:col-span-1 flex flex-col items-start justify-center">
+          <BiSolidQuoteAltLeft size={60} className="text-gray-400 mb-4 " />
+          <h2 className="text-4xl md:text-3xl content-title font-playfair font-bold text-gray-800 mb-6">
+            What our customers are saying
+          </h2>
+          <p className="text-[12px] italic text-gray-900 text-center mb-6">
+            Trusted journeys, proven performance.
+          </p>
+          <div className="flex gap-3">
+            <button
+              onClick={prevSlide}
+              className="p-2 rounded-full border hover:bg-gray-100"
+            >
+              <ChevronLeft size={22} />
+            </button>
+            <button
+              onClick={nextSlide}
+              className="p-2 rounded-full border hover:bg-gray-100"
+            >
+              <ChevronRight size={22} />
+            </button>
+          </div>
+        </div>
+
+        <div className="md:col-span-2 overflow-hidden">
+          <div
+            className="flex transition-transform duration-500 ease-in-out"
+            style={{
+              transform:
+                window.innerWidth <= 550
+                  ? `translateX(-${current * 40}%)`
+                  : `translateX(-${current * 28}%)`,
+              width:
+                window.innerWidth <= 550
+                  ? `${testimonials.length * 40}%`
+                  : `${testimonials.length * 28}%`,
+            }}
+          >
+            {testimonials.map((t, index) => (
+              <div
+                key={index}
+                className="px-2 flex-shrink-0 font-parkinsans text-[14px] leading-loose"
+                style={{
+                  width: window.innerWidth <= 700 ? "40%" : "28%",  
+                }}
+              >
+                <div className="bg-white rounded-2xl shadow p-6 text-left">
+                   <p className="font-semibold">{t.name}</p>
+                  <p className="text-sm text-gray-500 mb-4">{t.role}</p>
+ 
+                  <div className="flex mb-2">
+                    {Array.from({ length: t.stars }).map((_, i) => (
+                      <span key={i} className="text-green-500 text-lg">
+                        ★
+                      </span>
+                    ))}
+                  </div>
+ 
+                
+                  <p className="text-gray-700 leading-relaxed">{t.review}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+
+      </div>
     </section>
   );
 }
