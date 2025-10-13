ec478addb770a784f60c0862597bde74e74bb2cd
Sylvia Mumbi <mumbi099@gmail.com>
2025-10-13 21:06:35 +0300

    updated  blogs cover

--- src/components/resources/Blog.jsx
+++ src/components/resources/Blog.jsx
@@ -123,7 +123,7 @@ function Blog() {
   return (
     <div className="blog-section">
       {/* Hero Section */}
-      <div
+      {/* <div
         className="relative h-[300px] w-full bg-cover bg-center"
         style={{
           backgroundImage:
@@ -134,10 +134,25 @@ function Blog() {
         <h1 className="blog-topic text-5xl pt-[60px] font-bold text-white text-center absolute inset-0 flex items-center justify-center z-10">
           Insights and Articles
         </h1>
-      </div>
+      </div> */}
+      <div className="about-container bg-cover bg-center relative overflow-hidden h-[50vh] z-[-20]" style={{ backgroundImage: "url('https://i.pinimg.com/736x/83/4c/85/834c8525f9071792434d476fdb5fdd7e.jpg')" }} >
+          <div className="about-overlay absolute inset-0 bg-black/70"></div>         
+          <div className="absolute inset-0 flex flex-col items-center justify-center z-10 p-4">
+            <h1 className="text-5xl md:text-6xl font-extrabold text-white text-center leading-tight mb-4">
+              Insights and Articles
+            </h1>
+            <div className="text-center text-white text-lg font-semibold tracking-wide">
+                <Link to={`/blog`} className="font-semibold text-white hover:text-b-orange transition duration-300">
+                  Blogs
+                </Link>
+              <span className="mx-2 font-light opacity-80">|</span>
+              <span className="font-bold text-b-orange">Insights and Articles</span>
+            </div>
+          </div>
+        </div>
 
       {/* Blog List */}
-      <div className="blog-cont m-[30px] bg-white p-[13px] text-black">
+      <div className="blog-cont p-[13px] text-black mx-[30px] mt-[-90px] z-40 bg-white shadow-xl rounded-lg py-[30px] px-[50px] md:p-[70px]">
         <h5 className="font-semibold font-parkinsans text-[18px] px-[40px]">
           Recent Blog Posts
         </h5>
