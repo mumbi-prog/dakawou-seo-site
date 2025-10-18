65a8866492a0dc083a79208568dc257e435fe10e
Sylvia Mumbi <mumbi099@gmail.com>
2025-10-18 16:08:53 +0300

    Update meta info in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import NavBar from './components/NavBar';
-import HomePage from './components/HomePage';
-import GallerySection from './components/GallerySection';
-import AboutSection from './components/AboutSection';
-import ContactSection from './components/ContactSection';
-import Terms from './components/legal/Terms';
-import Privacy from './components/legal/Privacy';
-import ServicesSection from './components/services/ServicesSection';
-import Blog from './components/resources/Blog';
-import BlogDetails from './components/resources/BlogDetails';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import HomePage from "./components/HomePage";
+import GallerySection from "./components/GallerySection";
+import AboutSection from "./components/AboutSection";
+import ContactSection from "./components/ContactSection";
+import Terms from "./components/legal/Terms";
+import Privacy from "./components/legal/Privacy";
+import ServicesSection from "./components/services/ServicesSection";
+import Blog from "./components/resources/Blog";
+import BlogDetails from "./components/resources/BlogDetails";
 
 function App() {
   return (
